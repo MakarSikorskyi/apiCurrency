61178dcbaf2fa478659484d16c58db6e5c9a1595
Makar <fullmakar@gmail.com>
2024-03-06 16:26:54 +0400

    upd

--- app/GraphQL/Mutations/LinkMutator.php
+++ app/GraphQL/Mutations/LinkMutator.php
@@ -2,7 +2,44 @@
 
 namespace App\GraphQL\Mutations;
 
-class LinkMutator
-{
+use App\Models\Link;
+use Illuminate\Support\Str;
 
+class LinkMutator {
+    public function create($rootValue, array $args) {
+        $url = $args['url'];
+
+        $existingLink = Link::where('url', $url)->first();
+
+        if($existingLink) {
+            return $existingLink;
+        }
+
+        $shortenedUrl = $this->generateUniqueShortLink($url);
+
+        $link = Link::create([
+            'url' => $url,
+            'shortened_url' => $shortenedUrl,
+            'clicks' => 0,
+        ]);
+
+        return $link;
+    }
+
+    protected function generateUniqueShortLink($url = ''): string
+    {
+        do {
+            $shortenedUrl = $this->hashUrl($url);
+        } while (Link::where('shortened_url', $shortenedUrl)->exists());
+
+        return $shortenedUrl;
+    }
+
+    private function hashUrl($url)
+    {
+        $primaryHash = hash('crc32b', $url);
+        $combinedString = $primaryHash . Str::random(6);
+
+        return hash('crc32b', $combinedString);
+    }
 }
--- app/Services/CurrencyRateService.php
+++ app/Services/CurrencyRateService.php
@@ -4,5 +4,16 @@ namespace App\Services;
 
 class CurrencyRateService
 {
+    public function getRates($from, $to): array
+    {
+        // Тут должен был быть парсер :)
+        $rate = [
+            'USD' => [
+                'RUB' => 5,
+                'EUR' => 0.85
+            ]
+        ];
 
+        return $rate[$from][$to] || 0.85;
+    }
 }
